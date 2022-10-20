e3a42df468420179d6031ed77db67f24303317a1
MuhammadUsamaNazar <80358162+MuhammadUsamaNazar@users.noreply.github.com>
2022-10-20 20:39:38 +0500

    Update Chessboard.js

--- Chessboard.js
+++ Chessboard.js
@@ -1,42 +1,192 @@
-import { View, StyleSheet } from "react-native";
-function ChessBoard(props) {
-  const blocks = [];
-  for (let index = 1; index <= 8; index++) {
-    for (let yindex = 1; yindex <= 8; yindex++) {
-      blocks.push(
-        <View
-          style={
-            index % 2 == 0
-              ? yindex % 2 == 0
-                ? styles.bblock
-                : styles.gblock
-              : yindex % 2 != 0
-              ? styles.bblock
-              : styles.gblock
-          }
-        ></View>
-      );
-    }
-  }
-  return <View style={styles.board}>{blocks}</View>;
-}
+import React, { useState } from "react";
+import { View, TouchableOpacity, Text, StyleSheet } from "react-native";
+
+const FlexWrapLayout = () => {
+  const [flexWrap, setFlexWrap] = useState("wrap");
+
+  return (
+    <PreviewLayout
+      label="flexWrap"
+      selectedValue={flexWrap}
+      values={["wrap", "nowrap"]}
+      setSelectedValue={setFlexWrap}>
+      <View
+        style={[styles.box, { backgroundColor: "black" }]}
+
+      />
+      <View
+        style={[styles.box, { backgroundColor: "white" }]}
+      />
+      <View
+        style={[styles.box, { backgroundColor: "black" }]}
+      />
+      <View
+        style={[styles.box, { backgroundColor: "white" }]}
+      />
+      <View
+        style={[styles.box, { backgroundColor: "black" }]}
+      />
+      <View
+        style={[styles.box, { backgroundColor: "white" }]}
+      />
+      <View
+        style={[styles.box, { backgroundColor: "black" }]}
+      />
+      <View
+        style={[styles.box, { backgroundColor: "white" }]}
+      />
+      <View
+        style={[styles.box, { backgroundColor: "black" }]}
+      />
+      <View
+        style={[styles.box, { backgroundColor: "white" }]}
+      />
+      <View
+        style={[styles.box, { backgroundColor: "black" }]}
+      />
+      <View
+        style={[styles.box, { backgroundColor: "white" }]}
+      />
+      <View
+        style={[styles.box, { backgroundColor: "black" }]}
+      /><View
+      style={[styles.box, { backgroundColor: "white" }]}
+    />
+    <View
+        style={[styles.box, { backgroundColor: "black" }]}
+
+      />
+      <View
+        style={[styles.box, { backgroundColor: "white" }]}
+      />
+      <View
+        style={[styles.box, { backgroundColor: "black" }]}
+      />
+      <View
+        style={[styles.box, { backgroundColor: "white" }]}
+      />
+      <View
+        style={[styles.box, { backgroundColor: "black" }]}
+      />
+      <View
+        style={[styles.box, { backgroundColor: "white" }]}
+      />
+      <View
+        style={[styles.box, { backgroundColor: "black" }]}
+      />
+      <View
+        style={[styles.box, { backgroundColor: "white" }]}
+      />
+      <View
+        style={[styles.box, { backgroundColor: "black" }]}
+      />
+      <View
+        style={[styles.box, { backgroundColor: "white" }]}
+      />
+      <View
+        style={[styles.box, { backgroundColor: "black" }]}
+      />
+      <View
+        style={[styles.box, { backgroundColor: "white" }]}
+      />
+      <View
+        style={[styles.box, { backgroundColor: "black" }]}
+      /><View
+      style={[styles.box, { backgroundColor: "white" }]}
+    />
+    <View
+        style={[styles.box, { backgroundColor: "black" }]}
+      />
+
+   
+    </PreviewLayout>
+  );
+};
+
+const PreviewLayout = ({
+  label,
+  children,
+  values,
+  selectedValue,
+  setSelectedValue,
+}) => (
+  <View style={{ padding: 10, flex: 1 }}>
+    <Text style={styles.label}>{label}</Text>
+    <View style={styles.row}>
+      {values.map((value) => (
+        <TouchableOpacity
+          key={value}
+          onPress={() => setSelectedValue(value)}
+          style={[
+            styles.button,
+            selectedValue === value && styles.selected,
+          ]}
+        >
+          <Text
+            style={[
+              styles.buttonLabel,
+              selectedValue === value &&
+                styles.selectedLabel,
+            ]}
+          >
+            {value}
+          </Text>
+        </TouchableOpacity>
+      ))}
+    </View>
+    <View
+      style={[
+        styles.container,
+        { [label]: selectedValue },
+      ]}
+    >
+      {children}
+    </View>
+  </View>
+);
+
 const styles = StyleSheet.create({
-  board: {
-    width: 420,
-    height: 420,
-    borderColor: "brown",
+  container: {
+    flex: 1,
+    marginTop: 8,
+    backgroundColor: "aliceblue",
+    maxHeight: 400,
+  },
+  box: {
+    width: 50,
+    height: 80,
+  },
+  row: {
     flexDirection: "row",
     flexWrap: "wrap",
   },
-  gblock: {
-    width: 50,
-    height: 50,
-    backgroundColor: "grey",
+  button: {
+    paddingHorizontal: 8,
+    paddingVertical: 6,
+    borderRadius: 4,
+    backgroundColor: "oldlace",
+    marginHorizontal: "1%",
+    marginBottom: 6,
+    minWidth: "48%",
+    textAlign: "center",
   },
-  bblock: {
-    width: 50,
-    height: 50,
-    backgroundColor: "black",
+  selected: {
+    backgroundColor: "coral",
+    borderWidth: 0,
+  },
+  buttonLabel: {
+    fontSize: 12,
+    fontWeight: "500",
+    color: "coral",
+  },
+  selectedLabel: {
+    color: "white",
+  },
+  label: {
+    textAlign: "center",
+    marginBottom: 10,
+    fontSize: 24,
   },
 });
-export default ChessBoard;
+
+export default FlexWrapLayout;
